6bfa5c1f6902966774913afad38cb5b0e64808eb
Faaz Laeeq <faazlaeeq@gmail.com>
2024-08-27 14:04:43 +0500

    evn added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-, http = require('http');
+  , http = require('http');
 
 
 const app = express();
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 const logger = require('morgan');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
-var multer  = require('multer')
+var multer = require('multer')
 var cors = require('cors')
 require('dotenv').config();
 
@@ -20,15 +20,15 @@ app.use(bodyParser.json())
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-  
+
 app.use(cors(corsOptions));
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://yasirameen92:7tdBflE5MW0IkaCm@events.io065.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Middleware...
 app.use(logger('dev'))
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const event = require('./routes/eventRoutes')
 
-app.use('/api/event',event)
+app.use('/api/event', event)
 
 
 
@@ -45,48 +45,48 @@ app.use('/api/event',event)
 
 const imageStorage = multer.diskStorage({
   // Destination to store image     
-  destination: 'public', 
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '_' + Date.now() 
-           + path.extname(file.originalname))
-          // file.fieldname is name of the field (image)
-          // path.extname get the uploaded file extension
+  destination: 'public',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '_' + Date.now()
+      + path.extname(file.originalname))
+    // file.fieldname is name of the field (image)
+    // path.extname get the uploaded file extension
   }
 });
 
 const imageUpload = multer({
   storage: imageStorage,
   limits: {
-    fileSize: 5000000 
+    fileSize: 5000000
   },
   fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(png|jpg|pdf|jpeg)$/)) { 
-       // upload only png and jpg format
-       return cb(new Error('Please upload a Image'))
-     }
-   cb(undefined, true)
-}
-}) 
+    if (!file.originalname.match(/\.(png|jpg|pdf|jpeg)$/)) {
+      // upload only png and jpg format
+      return cb(new Error('Please upload a Image'))
+    }
+    cb(undefined, true)
+  }
+})
 
-app.post('/file/upload', imageUpload.single('image'),function (req, res) {
+app.post('/file/upload', imageUpload.single('image'), function (req, res) {
 
 
   const file = req.file
 
-   if(file) {
+  if (file) {
 
     const response = {
       status: "OK",
       result: {
-          success: true,
-          message: "File uploaded successfully.",
-          imageURL: "http://ec2-54-161-229-80.compute-1.amazonaws.com:3000/" + file.filename // Assuming `file.filename` is the name of the uploaded file.
+        success: true,
+        message: "File uploaded successfully.",
+        imageURL: "http://ec2-54-161-229-80.compute-1.amazonaws.com:3000/" + file.filename // Assuming `file.filename` is the name of the uploaded file.
       },
-  };
-  
-  return res.status(201).json(response);
+    };
 
-   }
+    return res.status(201).json(response);
+
+  }
 
 
 })
@@ -98,45 +98,49 @@ app.post('/file/upload', imageUpload.single('image'),function (req, res) {
 
 
 //Catch 404 Error and forward then to error handler..
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
 
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);});
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 //Error handler function...
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
 
-    const error = app.get('env') === 'development' ? err : {};
-    const status = err.status || 500;
+  const error = app.get('env') === 'development' ? err : {};
+  const status = err.status || 500;
 
-    //Respond to client..
-    res.status(status).json({
+  //Respond to client..
+  res.status(status).json({
 
-        success: false,
-        message: err
+    success: false,
+    message: err
 
 
-    });
+  });
 
-    console.error(err);
-  
-  }
+  console.error(err);
+
+}
 );
 
 function ensureToken(req, res, next) {
-        const bearerHeader = req.headers["authorization"];
-        if (typeof bearerHeader !== 'undefined') {
-          const bearer = bearerHeader.split(" ");
-          const bearerToken = bearer[1];
-          req.token = bearerToken;
-          next();
-        } else {
-          res.sendStatus(403);}}
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 //Start the sever..
 // const port = app.get('post') || 3000;
 // app.listen(port,() => console.log('Server is listening on port' + port));
 
-server.listen(app.get('port'), function() {
-console.log('Node app is running on port', app.get('port'));});
+server.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
